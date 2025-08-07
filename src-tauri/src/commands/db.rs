4da16a04136f9509c25b1b30d6731693c3cf09f5
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-08-07 01:57:44 -0400

    impl db initializtation

--- src-tauri/src/commands/db.rs
+++ src-tauri/src/commands/db.rs
@@ -1,1 +1,15 @@
+use rusqlite::{Connection, Result as SqlResult};
 
+pub fn init_db() -> SqlResult<Connection, String> {
+    let cache_dir = dirs::cache_dir()
+        .unwrap_or_else(|| std::env::temp_dir())
+        .join("com.extents.cache");
+
+    std::fs::create_dir_all(&cache_dir).ok();
+
+    let db_path = cache_dir.join("db.sqlite");
+
+    let connection = Connection::open(db_path).map_err(|e| e.to_string())?;
+
+    Ok(connection)
+}
