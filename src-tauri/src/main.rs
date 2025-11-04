67ae47fc2f4d56ef558f85fcca2dcabb323b9aa3
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2025-11-04 16:29:01 -0500

    changed app name to extents

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use app_lib::commands::logging::init_logging;
+use extents_lib::commands::logging::init_logging;
 
 fn main() {
     init_logging();
-    app_lib::run();
+    extents_lib::run();
 }
