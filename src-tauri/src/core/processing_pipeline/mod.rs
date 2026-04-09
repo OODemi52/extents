71117967eb92e7e81068a1a47cf7bc0c77f40936
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2026-04-09 16:45:21 -0400

    update mod

--- src-tauri/src/core/processing_pipeline/mod.rs
+++ src-tauri/src/core/processing_pipeline/mod.rs
@@ -4,5 +4,6 @@ mod ingest;
 mod normalize;
 pub mod types;
 
+pub use display::{render_for_display, DisplayImage, DisplayMode};
 pub use ingest::ingest_from_path;
 pub use types::{AlphaPlane, ImageDimensions, ProcessingPipelineImage, RgbPixel, WorkingImage};
