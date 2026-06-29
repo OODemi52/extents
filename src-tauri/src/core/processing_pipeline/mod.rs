28efc85a499b8c05de3107d21226448ec281e6bf
Demilade Daniel-Akanle <76265069+OODemi52@users.noreply.github.com>
2026-06-29 15:52:09 -0400

    chore: remove legacy CPU processing pipeline

--- src-tauri/src/core/processing_pipeline/mod.rs
+++
@@ -1,9 +0,0 @@
-mod decode;
-mod ingest;
-mod luts;
-mod normalize;
-pub mod types;
-
-pub use crate::core::image::ImageDimensions;
-pub use ingest::ingest_from_path;
-pub use types::{AlphaPlane, ProcessingPipelineImage, RgbPixel, WorkingImage};
--- src-tauri/src/core/processing_pipeline/normalize/raw_development/mod.rs
+++
@@ -1,8 +0,0 @@
-mod color_matrix;
-mod color_transform;
-mod develop;
-mod highlight_reconstruction;
-mod types;
-mod white_balance;
-
-pub(in crate::core::processing_pipeline::normalize) use develop::develop_working_image;
